29c286653341cc72c8a97b830451d095718cb57f
charlesturner7c5 <charlesturner7c5@gmail.com>
2015-05-30 19:28:06 +0100

    First pass of personal changes.

---
+++ clang/path.zsh
@@ -0,0 +1,3 @@
+export LLVM_SRC=$HOME/src/llvm
+export LLVM_BLD=$HOME/src/llvm-build
+export PATH="$LLVM_BUILD/bin:$PATH"
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -7,4 +7,16 @@ then
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
+  # TODO: alias find to GNU find
 fi
+
+alias e='emacsclient -n'
+alias m='more'
+alias j='jobs'
+alias pd='pushd .'
+alias pop='popd'
+alias ack="ack-grep --pager='less -R'"
+alias gdb='gdb -q'
+alias dups='find -not -empty -type f -printf "%s\n" | sort -rn | uniq -d | xargs -I{} -n1 find -type f -size {}c -print0 | xargs -0 sha1sum | sort | uniq -w64'
+alias disas='llvm-objdump-3.5 -disassemble -s -t'
+alias show_relocations='llvm-readobj-3.5 -relocations'
