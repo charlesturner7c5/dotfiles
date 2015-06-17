88ec8baa306df00e1739bbd3eb1b86c27c559af2
Charlie Turner <charlie@alan.madeup.com>
2015-06-17 06:26:02 +0100

    Move "system" aliases to my Clang file.

---
+++ clang/aliases.zsh
@@ -0,0 +1,2 @@
+alias disas='llvm-objdump-3.5 -disassemble -s -t'
+alias show_relocations='llvm-readobj-3.5 -relocations'
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -2,6 +2,9 @@ alias ls="ls -F --color"
 alias l="ls -lAh --color"
 alias ll="ls -l --color"
 alias la='ls -A --color'
+alias grep='grep --color=auto'
+alias ackp="ack --color-match=green --pager='less -R'"
+alias ack='ack --color-match=green'
 alias e='emacsclient -n'
 alias m='less'
 alias j='jobs'
@@ -10,5 +13,4 @@ alias pop='popd'
 alias ack="ack-grep --pager='less -R'"
 alias gdb='gdb -q'
 alias dups='find -not -empty -type f -printf "%s\n" | sort -rn | uniq -d | xargs -I{} -n1 find -type f -size {}c -print0 | xargs -0 sha1sum | sort | uniq -w64'
-alias disas='llvm-objdump-3.5 -disassemble -s -t'
-alias show_relocations='llvm-readobj-3.5 -relocations'
+
