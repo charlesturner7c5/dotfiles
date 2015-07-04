615fa28efebe96dbfc36928dee9d97b494b47e95
Charlie Turner <charlesturner7c5@gmail.com>
2015-07-04 15:54:29 +0100

    Point out the reload problem.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,4 @@
+# FIXME: This isn't a very good idea, you get global warming each time
+# you reload. This needs to be smarter about updating environment
+# variables for example.
 alias reload!='. ~/.zshrc'
